a909ce8a0003770244f0db06b13d847b423eb82b
Grovre <landonvee111@gmail.com>
2024-02-22 01:25:46 -0600

    economy class update

--- src/github/grovre/Economy.java
+++ src/github/grovre/Economy.java
@@ -1,4 +1,23 @@
 package github.grovre;
 
+import github.grovre.markets.Market;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public class Economy {
+    final Set<Market> markets;
+
+    public Economy() {
+        markets = new HashSet<>();
+    }
+
+    public Economy(Market... markets) {
+        this.markets = Arrays.stream(markets).collect(Collectors.toSet());
+        // If Collectors.toSet impl ever changes for any reason
+        assert this.markets instanceof HashSet<Market>
+                : "this.markets not instanceof HashSet<>";
+    }
 }
--- src/github/grovre/markets/MarketProduct.java
+++ src/github/grovre/markets/MarketProduct.java
@@ -7,6 +7,9 @@ public record MarketProduct(String name, UUID id) implements Serializable {
 
     @Override
     public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        
         if (!(obj instanceof MarketProduct other))
             return false;
 
