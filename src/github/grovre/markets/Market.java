37ba85fbf8662c722b36aa5c817a2e4e9309a8cb
Grovre <landonvee111@gmail.com>
2024-02-22 00:53:43 -0600

    SortedSet instead of ArrayList

--- src/github/grovre/markets/Market.java
+++ src/github/grovre/markets/Market.java
@@ -3,43 +3,41 @@ package github.grovre.markets;
 import github.grovre.transactions.Buy;
 import github.grovre.transactions.Sale;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Market {
 
     final MarketProduct product;
-    final ArrayList<Buy> closedBuys;
-    final ArrayList<Sale> closedSales;
-    final ArrayList<Buy> buyRequests;
-    final ArrayList<Sale> saleRequests;
+    final SortedSet<Buy> closedBuys;
+    final SortedSet<Sale> closedSales;
+    final SortedSet<Buy> buyRequests;
+    final SortedSet<Sale> saleRequests;
 
     public Market(MarketProduct product) {
         this.product = product;
-        closedBuys = new ArrayList<>();
-        closedSales = new ArrayList<>();
-        buyRequests = new ArrayList<>();
-        saleRequests = new ArrayList<>();
+        closedBuys = new TreeSet<>();
+        closedSales = new TreeSet<>();
+        buyRequests = new TreeSet<>();
+        saleRequests = new TreeSet<>();
     }
 
     public MarketProduct getProduct() {
         return product;
     }
 
-    public List<Buy> getClosedBuys() {
-        return Collections.unmodifiableList(closedBuys);
+    public SortedSet<Buy> getClosedBuys() {
+        return Collections.unmodifiableSortedSet(closedBuys);
     }
 
-    public List<Sale> getClosedSales() {
-        return Collections.unmodifiableList(closedSales);
+    public SortedSet<Sale> getClosedSales() {
+        return Collections.unmodifiableSortedSet(closedSales);
     }
 
-    public List<Buy> getBuyRequests() {
-        return Collections.unmodifiableList(buyRequests);
+    public SortedSet<Buy> getBuyRequests() {
+        return Collections.unmodifiableSortedSet(buyRequests);
     }
 
-    public List<Sale> getSaleRequests() {
-        return Collections.unmodifiableList(saleRequests);
+    public SortedSet<Sale> getSaleRequests() {
+        return Collections.unmodifiableSortedSet(saleRequests);
     }
 }
--- src/github/grovre/transactions/Transaction.java
+++ src/github/grovre/transactions/Transaction.java
@@ -1,13 +1,31 @@
 package github.grovre.transactions;
 
+import java.util.Objects;
 import java.util.UUID;
 
-public class Transaction {
+public class Transaction implements Comparable<Transaction> {
     public final double amount;
     public final UUID id;
 
     public Transaction(double amount, UUID id) {
         this.amount = amount;
         this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Transaction that)) return false;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
+
+    @Override
+    public int compareTo(Transaction o) {
+        return Double.compare(amount, o.amount);
     }
 }
